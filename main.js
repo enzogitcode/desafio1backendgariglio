998fd791621dfe3bf3812f3c014a91d506b31480
enzogit <enzo_91_10@hotmail.com>
2024-02-24 15:29:37 -0300

    constructor

---
+++ main.js
@@ -0,0 +1,14 @@
+class ProductManager {
+    constructor() {
+        this.title= title,
+        this.description= description,
+        this.price= price,
+        this.thumnnail= thumnail,
+        this.code= code,
+        this.stock= stock
+    }
+}
+//método getProduct
+
+//método addProduct
+
