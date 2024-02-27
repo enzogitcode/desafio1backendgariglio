9cc495b29a93641b7e8d3dc63c319a5372d73825
enzogit <enzo_91_10@hotmail.com>
2024-02-27 17:54:12 -0300

    nvo commit

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 class ProductManager {
+    static id=0
     constructor() {
-        this.title= title,
-        this.description= description,
-        this.price= price,
-        this.thumnnail= thumnail,
-        this.code= code,
-        this.stock= stock
+        this.products = []
     }
+    addProduct () {
+        this.title = title,
+   this.description = description,
+   this.price = price,
+   this.thumnnail = thumnail,
+   this.code = code,
+   this.stock = stock
+    }
+    getProducts () {}
+    getProductById () {}
 }
-//método getProduct
+
+
+/* this.title = title,
+   this.description = description,
+   this.price = price,
+   this.thumnnail = thumnail,
+   this.code = code,
+   this.stock = stock */
+    
+    //método getProduct
 
 //método addProduct
 
+//método getProductbyId
