be90e9e9d1addc3183205cf33b5f3248a4e1b346
enzogit <enzo_91_10@hotmail.com>
2024-02-27 20:33:20 -0300

    completando métodos

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 class ProductManager {
-    static id=0
     constructor() {
         this.products = []
     }
-    addProduct () {
-        this.title = title,
-   this.description = description,
-   this.price = price,
-   this.thumnnail = thumnail,
-   this.code = code,
-   this.stock = stock
+    static id=0
+    addProduct (title, description, price, thumbnail, code, stock) {
+        ProductManager.id++
+         this.products.push ({ id:ProductManager.id, title, description, price, thumbnail, code, stock })
     }
-    getProducts () {}
+    getProducts (products) {
+    return  new products
+    }
     getProductById () {}
 }
+
+const products= new ProductManager;
+productos.addProduct= ('producto prueba', 'Este es un producto prueba', 200, 'sin imagen', 'abc123', 25)
 
 
 /* this.title = title,
    this.description = description,
    this.price = price,
-   this.thumnnail = thumnail,
+   this.thumbnail = thumbail,
    this.code = code,
    this.stock = stock */
     
